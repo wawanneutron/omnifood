9c6d7421ebc38a1fee1d3538394da84106818810
wawanneutron <wawanneutron1331@gmail.com>
2022-06-24 09:03:43 +0700

    js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,63 @@
+// * current yuear in footer
+const year = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+year.textContent = currentYear;
+
+// * Make mobile Navigation works
+const header = document.querySelector(".header");
+const btnNav = document.querySelector(".btn-mobile-nav");
+
+btnNav.addEventListener("click", () => {
+  header.classList.toggle("nav-open");
+});
+
+//  * Smoth scroling animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    const href = link.getAttribute("href");
+    console.log(href);
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      header.classList.toggle("nav-open");
+    }
+  });
+});
+
+// * Sticky Navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  (entries) => {
+    const ent = entries[0].isIntersecting;
+    console.log(ent);
+    // if false show sticky navbar
+    if (!ent) document.body.classList.add("sticky");
+    // but true remove sticky
+    else document.body.classList.remove("sticky");
+  },
+  {
+    // in the view port
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
@@ -9,7 +69,8 @@ function checkFlexGap() {
   flex.appendChild(document.createElement("div"));
 
   document.body.appendChild(flex);
-  var isSupported = flex.scrollHeight === 1;
+  let isSupported = flex.scrollHeight === 1;
+  console.log(`${isSupported}`);
   flex.parentNode.removeChild(flex);
   console.log(isSupported);
 
@@ -18,10 +79,3 @@ function checkFlexGap() {
 checkFlexGap();
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
-
-const header = document.querySelector("header");
-const btnNav = header.lastElementChild;
-
-btnNav.addEventListener("click", () => {
-  header.classList.toggle("nav-open");
-});
